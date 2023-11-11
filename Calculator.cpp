0a97c4feb163d6083780e1742eb8b6b4fb9fe70b
KaranoSoraa <kozlov.2013@mail.ru>
2023-11-11 11:49:40 +0300

    Calculator

---
+++ Calculator.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL, "RU");
+	int x, y;
+	cout << "Введите два числа: ";
+	cin >> x >> y;
+	char oper;
+	cout << "Введите операцию (+, -, *, /, %): ";
+	cin >> oper;
+	switch (oper) {
+	case '+':
+		cout << x + y;
+		break;
+	case '-':
+		cout << x - y;
+		break;
+	case '*':
+		cout << x * y;
+		break;
+	case '/':
+		if (y != 0) cout << x / y;
+		else cout << "у равен нулю.";
+		break;
+	case '%':
+		cout << x % y;
+		break;
+	default: cout << "Неизвестная ошибка";
+	}
+}
